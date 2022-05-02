7a61ddf9403f79d71fca655dfa1be355f9383359
Diogo Martins <diogofied@gmail.com>
2022-05-02 16:36:27 -0300

    Corrige erros de digitação

--- ex4.js
+++ ex4.js
@@ -3,7 +3,7 @@ let numSize = parseInt(prompt('Quantos número deseja comparar?', 6));
 let numArr = [];
 
 // criar estrutura de repetição para atribuir os número no array
-for (let i = 0; i < numSize; i++) { 
+for (let i = 0; i < numSize; i++) {
   numArr[i] = parseInt(prompt(`Digite o valor do #${i + 1} número:`, Math.round(Math.random() * 50)));
 }
 
@@ -14,14 +14,14 @@ let sortArr = [];
 const lowerNum = numArr => {
   sortArr = numArr;
   for (let i = 1; i < numSize; i++) {
-    for (let j = 0; j < i ; j++) {
+    for (let j = 0; j < i; j++) {
       if (sortArr[i] < sortArr[j]) {
-        [sortArr[j], sortArr[i]] = [sortArr[i], sortArr[j]]
+        [sortArr[j], sortArr[i]] = [sortArr[i], sortArr[j]];
       }
     }
   }
 
   return sortArr[0];
-}
+};
 // exibir o menor número
 lowerNum(numArr);
