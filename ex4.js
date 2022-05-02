bc1ac686645e5a60548edc7a6f35c81b7863b961
Diogo Martins <diogofied@gmail.com>
2022-05-02 15:32:32 -0300

    Finaliza exercício 4

---
+++ ex4.js
@@ -0,0 +1,27 @@
+// criar array para armazenar uma sequência de números
+let numSize = parseInt(prompt('Quantos número deseja comparar?', 6));
+let numArr = [];
+
+// criar estrutura de repetição para atribuir os número no array
+for (let i = 0; i < numSize; i++) { 
+  numArr[i] = parseInt(prompt(`Digite o valor do #${i + 1} número:`, Math.round(Math.random() * 50)));
+}
+
+console.log(numArr);
+
+// criar função que compare os valores e retorne uma nova array em ordem crescente
+let sortArr = [];
+const lowerNum = numArr => {
+  sortArr = numArr;
+  for (let i = 1; i < numSize; i++) {
+    for (let j = 0; j < i ; j++) {
+      if (sortArr[i] < sortArr[j]) {
+        [sortArr[j], sortArr[i]] = [sortArr[i], sortArr[j]]
+      }
+    }
+  }
+
+  return sortArr[0];
+}
+// exibir o menor número
+lowerNum(numArr);
